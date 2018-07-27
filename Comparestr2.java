fddcee33bf8abec0c17d1bdc3996fcddca0a8eb3
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-27 14:27:43 +0530

    Update Comparestr2.java

--- Comparestr2.java
+++ Comparestr2.java
@@ -4,8 +4,8 @@ public class Comparestr2 {
 	public static void main(String[] args) {
 		String s1,s2;
 Scanner c=new Scanner(System.in);
-s1=c.nextLine();
-s2=c.nextLine();
+s1=c.next();
+s2=c.next();
 if(s1.length()==s2.length())
 {
 	System.out.println(s1);
