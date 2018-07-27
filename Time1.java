feb161852d4ad32185705e1186d8837acfe7b138
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-27 13:35:26 +0530

    Add files via upload

---
+++ Time1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Time1 {
+
+	public static void main(String[] args) {
+int n;
+Scanner c=new Scanner(System.in);
+String s=c.next();
+n=c.nextInt();
+for(int i=0;i<n;i++)
+{
+	System.out.println(s);
+}
+}
+
+}
