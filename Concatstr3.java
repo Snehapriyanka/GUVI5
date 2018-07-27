fc98e771cb38c153162952c36297e1b5a15c3916
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-27 14:25:08 +0530

    Add files via upload

---
+++ Concatstr3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Concatstr3 {
+
+	public static void main(String[] args) {
+		String x,y,d;
+Scanner sc=new Scanner(System.in);
+x=sc.next();
+y=sc.next();
+d=x+y;
+System.out.println(d);
+	}
+
+}
